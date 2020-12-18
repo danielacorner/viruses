47fa49f6e82e53dc2a39e6856203a157cd4d4b8b
Daniel Corner <danielcorner7@gmail.com>
2020-12-18 14:52:54 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Canvas } from "react-three-fiber";
+import { Physics, usePlane, useBox } from "@react-three/cannon";
 
+function Plane(props) {
+  const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0], ...props }));
+  return (
+    <mesh ref={ref}>
+      <planeBufferGeometry attach="geometry" args={[100, 100]} />
+    </mesh>
+  );
+}
+
+function Cube(props) {
+  const [ref] = useBox(() => ({ mass: 1, position: [0, 5, 0], ...props }));
+  return (
+    <mesh ref={ref}>
+      <boxBufferGeometry attach="geometry" />
+    </mesh>
+  );
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas>
+        <Physics>
+          <Plane />
+          <Cube />
+        </Physics>
+      </Canvas>
     </div>
   );
 }
