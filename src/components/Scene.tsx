f3d4fe226db1db272aa6e6d7b911505882796e47
Daniel Corner <danielcorner7@gmail.com>
2020-12-19 03:11:57 -0500

    refactor

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -9,6 +9,7 @@ import { useStore } from "../store";
 import JitteryParticle from "./Shapes/JitteryParticle";
 import ModelActivatorProtein from "./GLTFs/activator_protein-1";
 import SarsCov2 from "./GLTFs/SarsCov2";
+import Model1bv1 from "./GLTFs/1bv1";
 
 const getRandStartPosition = (min, max) => [
   randBetween(min, max),
@@ -25,15 +26,28 @@ const Scene = () => {
   const numParticles = useControl("particles", {
     type: "number",
     min: 1,
-    max: 100,
+    max: 50,
     value: 10,
   });
-  const worldSize = useStore((state) => state.worldSize);
+  const worldRadius = useStore((state) => state.worldRadius);
 
-  const positions = {
-    covid: getRandPositionsArr(numParticles, worldSize),
-    protein1: getRandPositionsArr(numParticles, worldSize),
-  };
+  const proteins = [
+    {
+      particle: SarsCov2,
+      scale: 0.005,
+      positions: getRandPositionsArr(numParticles, worldRadius / 2),
+    },
+    {
+      particle: Model1bv1,
+      scale: 0.005,
+      positions: getRandPositionsArr(numParticles, worldRadius / 2),
+    },
+    {
+      particle: ModelActivatorProtein,
+      scale: 0.005,
+      positions: getRandPositionsArr(numParticles, worldRadius / 2),
+    },
+  ];
   return (
     <>
       <OrbitControls />
@@ -53,29 +67,22 @@ const Scene = () => {
         // allowSleep={false}
       >
         {/* <Plane /> */}
-        {positions.covid.map((pos) => (
-          // instance performance https://codesandbox.io/embed/r3f-instanced-colors-8fo01
-          // <instancedMesh key={JSON.stringify(pos)}>
-          <instancedMesh key={JSON.stringify(pos)}>
-            <JitteryParticle
-              key={JSON.stringify(pos)}
-              ChildParticle={SarsCov2}
-              position={pos}
-              scale={0.005}
-            />
-          </instancedMesh>
+        {proteins.map(({ particle, scale, positions }, idx) => (
+          <React.Fragment key={idx}>
+            {positions.map((pos) => (
+              // instance performance https://codesandbox.io/embed/r3f-instanced-colors-8fo01
+              // <instancedMesh key={JSON.stringify(pos)}>
+              <instancedMesh key={JSON.stringify(pos)}>
+                <JitteryParticle
+                  // key={JSON.stringify(pos)}
+                  ChildParticle={particle}
+                  position={pos}
+                  scale={0.005}
+                />
+              </instancedMesh>
+            ))}
+          </React.Fragment>
         ))}
-        {positions.protein1.map((pos) => (
-          // instance performance https://codesandbox.io/embed/r3f-instanced-colors-8fo01
-          <instancedMesh key={JSON.stringify(pos)}>
-            <JitteryParticle
-              position={pos}
-              ChildParticle={ModelActivatorProtein}
-              scale={0.005}
-            />
-          </instancedMesh>
-        ))}
-
         <Walls />
       </Physics>
       {/* <Effects /> */}
--- src/components/Walls.tsx
+++ src/components/Walls.tsx
@@ -6,52 +6,51 @@ import { Plane } from "./Shapes/Plane";
 // https://raw.githubusercontent.com/Jam3/nice-color-palettes/HEAD/visualize/1000.png
 // const palette = niceColors[6]; // e.g. => [ "#69d2e7", "#a7dbd8", "#e0e4cc", "#f38630", "#fa6900" ]
 export function Walls() {
-  const worldSize = useStore((state) => state.worldSize);
+  const worldRadius = useStore((state) => state.worldRadius);
   // const palette = ["#69d2e7", "#a7dbd8", "#e0e4cc", "#f38630", "#fa6900"];
-  // const rotFrontBack =
   const walls = [
     // {/* in front */}
     {
       rotation: [0 * Math.PI, 0, 0],
       color: "#27d3ca",
-      position: [0, -0, -worldSize],
+      position: [0, -0, -worldRadius],
     },
     // {/* behind */}
     {
       rotation: [0, -1 * Math.PI, 0],
       color: "#e72747",
-      position: [0, -0, worldSize],
+      position: [0, -0, worldRadius],
     },
     // {/* left */}
     {
       rotation: [0, 0.5 * Math.PI, 0],
       color: "#f38630",
-      position: [-worldSize, 0, 0],
+      position: [-worldRadius, 0, 0],
     },
     // {/* right */}
     {
       rotation: [0, -0.5 * Math.PI, 0],
       color: "#f38630",
-      position: [worldSize, -0, 0],
+      position: [worldRadius, -0, 0],
     },
     // {/* floor */}
     {
       rotation: [-0.5 * Math.PI, 0, 0],
       color: "#d5fd8b",
-      position: [0, -worldSize, 0],
+      position: [0, -worldRadius, 0],
     },
     // {/* ceiling */}
     {
       rotation: [0.5 * Math.PI, 0, 0],
       color: "#ab3deb",
-      position: [0, worldSize, 0],
+      position: [0, worldRadius, 0],
     },
   ];
 
   return (
     <>
       {walls.map((props) => (
-        <Plane {...props} width={worldSize * 2} height={worldSize * 2} />
+        <Plane {...props} width={worldRadius * 2} height={worldRadius * 2} />
       ))}
     </>
   );
