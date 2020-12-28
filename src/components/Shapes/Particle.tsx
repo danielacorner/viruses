2ce9a27329126a708c1d77123b46c6e9b9dc35d2
Daniel Corner <danielcorner7@gmail.com>
2020-12-28 02:18:53 -0500

    checkpoint

--- src/components/Shapes/Particle.tsx
+++ src/components/Shapes/Particle.tsx
@@ -35,11 +35,6 @@ const Particle = ({
   jittery,
   ...rest
 }) => {
-  // const coords = useMemo(
-  //   () => [...new Array(numParticles)].map(() => [rpi(), rpi(), rpi()]),
-  //   [numParticles]
-  // );
-
   const worldRadius = useStore((state) => state.worldRadius);
 
   // https://codesandbox.io/s/may-with-60fps-your-web-site-run-xj31x?from-embed=&file=/src/index.js:297-1112
@@ -57,17 +52,25 @@ const Particle = ({
     ref: ref,
   });
 
-  // const dummy = new THREE.Object3D();
-  // useFrame((state) => {
-  //   const t = state.clock.getElapsedTime();
-  //   coords.forEach(([x, y, z], i) => {
-  //     dummy.rotation.set(x + t, y + t, z + t);
-  //     //       dummy.updateMatrix(void dummy.rotation.set(x + t, y + t, z + t))
-  //     dummy.updateMatrix();
-  //     mesh.current.setMatrixAt(i, dummy.matrix);
-  //   });
-  //   mesh.current.instanceMatrix.needsUpdate = true;
-  // });
+  const dummy = new THREE.Object3D();
+  const mesh = useRef(null as any);
+  const coords = useMemo(
+    () => [...new Array(numParticles)].map(() => [rpi(), rpi(), rpi()]),
+    [numParticles]
+  );
+  useFrame((state) => {
+    if (!(ref.current as any)?.setMatrixAt) {
+      return;
+    }
+    const t = state.clock.getElapsedTime();
+    coords.forEach(([x, y, z], i) => {
+      dummy.rotation.set(x + t, y + t, z + t);
+      //       dummy.updateMatrix(void dummy.rotation.set(x + t, y + t, z + t))
+      dummy.updateMatrix();
+      (ref.current as any).setMatrixAt(i, dummy.matrix);
+    });
+    (ref.current as any).instanceMatrix.needsUpdate = true;
+  });
 
   // random start positions
   useMount(() => {
