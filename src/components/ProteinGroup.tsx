438f09f5081829fac5d52e998b5078955476172b
dcorn068 <daniel@raventelemetry.com>
2021-01-03 15:13:53 -0500

    renderorder 3 on SingleParticle

--- src/components/ProteinGroup.tsx
+++ src/components/ProteinGroup.tsx
@@ -5,68 +5,68 @@ import { getRandStartPosition } from "./Shapes/particleUtils";
 import { useStore } from "../store";
 
 const ProteinGroup = ({
-  particle,
-  particleName,
-  interactive,
-  pathToGLTF,
-  pathToImage,
-  numIcosahedronFaces,
-  instanced = true,
-  mass,
+	particle,
+	particleName,
+	interactive,
+	pathToGLTF,
+	pathToImage,
+	numIcosahedronFaces,
+	instanced = true,
+	mass,
 }) => {
-  const numParticlesFloat: number = useControl(particleName, {
-    group: "Particles",
-    type: "number",
-    min: 1,
-    max: 100,
-    value: 3,
-  });
-  const numParticles = Math.ceil(numParticlesFloat);
+	const numParticlesFloat: number = useControl(particleName, {
+		group: "Particles",
+		type: "number",
+		min: 1,
+		max: 100,
+		value: 1,
+	});
+	const numParticles = Math.ceil(numParticlesFloat);
 
-  const worldRadius = useStore((state) => state.worldRadius);
+	const worldRadius = useStore((state) => state.worldRadius);
 
-  const [positionsArray, setPositionsArray] = useState(() =>
-    [...new Array(numParticles)].map(() => getRandStartPosition(worldRadius))
-  );
+	const [positionsArray, setPositionsArray] = useState(() =>
+		[...new Array(numParticles)].map(() => getRandStartPosition(worldRadius))
+	);
 
-  // change the positions array when numParticles changes;
-  // do this manually so that existing particles don't re-render & maintain their positions
-  useEffect(() => {
-    const numNewParticles = numParticles - positionsArray.length;
+	// change the positions array when numParticles changes;
+	// do this manually so that existing particles don't re-render & maintain their positions
+	useEffect(() => {
+		const numNewParticles = numParticles - positionsArray.length;
 
-    if (numNewParticles < 0) {
-      // slice off any excess,
-      const newPositionsArray = positionsArray
-        .slice(0, numParticles)
-        .filter(Boolean);
-      setPositionsArray(newPositionsArray);
-    } else if (numNewParticles > 0) {
-      // or populate any missing
-      const newPositionsArray = [...new Array(numNewParticles)].map(() =>
-        getRandStartPosition(worldRadius)
-      );
-      setPositionsArray((prev) => [...prev, ...newPositionsArray]);
-    }
-  }, [numParticles, positionsArray, worldRadius]);
+		if (numNewParticles < 0) {
+			// slice off any excess,
+			const newPositionsArray = positionsArray
+				.slice(0, numParticles)
+				.filter(Boolean);
+			setPositionsArray(newPositionsArray);
+		} else if (numNewParticles > 0) {
+			// or populate any missing
+			const newPositionsArray = [...new Array(numNewParticles)].map(() =>
+				getRandStartPosition(worldRadius)
+			);
+			setPositionsArray((prev) => [...prev, ...newPositionsArray]);
+		}
+	}, [numParticles, positionsArray, worldRadius]);
 
-  return (
-    <>
-      {positionsArray.map((position) => (
-        <SingleParticle
-          key={JSON.stringify(position)}
-          {...{
-            position,
-            pathToGLTF,
-            pathToImage,
-            numIcosahedronFaces,
-            mass,
-            interactive,
-            ChildParticle: particle,
-          }}
-        />
-      ))}
-    </>
-  );
+	return (
+		<>
+			{positionsArray.map((position) => (
+				<SingleParticle
+					key={JSON.stringify(position)}
+					{...{
+						position,
+						pathToGLTF,
+						pathToImage,
+						numIcosahedronFaces,
+						mass,
+						interactive,
+						ChildParticle: particle,
+					}}
+				/>
+			))}
+		</>
+	);
 };
 
 export default ProteinGroup;
--- src/components/Shapes/SingleParticle.tsx
+++ src/components/Shapes/SingleParticle.tsx
@@ -5,88 +5,93 @@ import { useStore } from "../../store";
 import * as THREE from "three";
 
 export function SingleParticle({
-  ChildParticle,
-  position,
-  numIcosahedronFaces,
-  pathToImage,
-  mass,
-  interactive,
-  pathToGLTF,
+	ChildParticle,
+	position,
+	numIcosahedronFaces,
+	pathToImage,
+	mass,
+	interactive,
+	pathToGLTF,
 }) {
-  // TODO: make NonInteractiveParticle instanced for better performance?
-  // TODO: make InteractiveParticle instanced for better performance?
-  const Particle = interactive ? InteractiveParticle : NonInteractiveParticle;
-  return (
-    <Particle
-      {...{
-        mass,
-        position,
-        numIcosahedronFaces,
-        pathToImage,
-        ChildParticle,
-        pathToGLTF,
-      }}
-    />
-  );
+	// TODO: make NonInteractiveParticle instanced for better performance?
+	// TODO: make InteractiveParticle instanced for better performance?
+	const Particle = interactive ? InteractiveParticle : NonInteractiveParticle;
+	return (
+		<Particle
+			{...{
+				mass,
+				position,
+				numIcosahedronFaces,
+				pathToImage,
+				ChildParticle,
+				pathToGLTF,
+			}}
+		/>
+	);
 }
 /** interacts with other particles using @react-three/cannon */
 function InteractiveParticle({
-  pathToGLTF,
-  position,
-  ChildParticle,
-  pathToImage,
-  mass,
-  numIcosahedronFaces,
+	pathToGLTF,
+	position,
+	ChildParticle,
+	pathToImage,
+	mass,
+	numIcosahedronFaces,
 }) {
-  // TODO:
-  // const temperature=useStore(state=>state.temperature)
+	// TODO:
+	// const temperature=useStore(state=>state.temperature)
 
-  // https://codesandbox.io/s/r3f-convex-polyhedron-cnm0s?from-embed=&file=/src/index.js:1639-1642
+	// https://codesandbox.io/s/r3f-convex-polyhedron-cnm0s?from-embed=&file=/src/index.js:1639-1642
 
-  const detail = Math.ceil(numIcosahedronFaces / 20);
-  const [ref] = useConvexPolyhedron(() => ({
-    mass,
-    position,
-    // https://threejs.org/docs/scenes/geometry-browser.html#IcosahedronBufferGeometry
-    args: new THREE.IcosahedronGeometry(1, detail),
-  }));
-  useJitterParticle({
-    mass,
-    ref,
-  });
-  const scale = useStore((state) => state.scale);
-  const set = useStore((state) => state.set);
+	const detail = Math.ceil(numIcosahedronFaces / 20);
+	const [ref] = useConvexPolyhedron(() => ({
+		mass,
+		position,
+		// https://threejs.org/docs/scenes/geometry-browser.html#IcosahedronBufferGeometry
+		args: new THREE.IcosahedronGeometry(1, detail),
+	}));
+	useJitterParticle({
+		mass,
+		ref,
+	});
+	const scale = useStore((state) => state.scale);
+	const set = useStore((state) => state.set);
 
-  return (
-    <mesh
-      ref={ref}
-      scale={[scale, scale, scale]}
-      onPointerDown={() => set({ selectedProtein: { pathToImage } })}
-    >
-      <ChildParticle />
-    </mesh>
-  );
+	return (
+		<mesh
+			ref={ref}
+			scale={[scale, scale, scale]}
+			onPointerDown={() => set({ selectedProtein: { pathToImage } })}
+		>
+			<ChildParticle />
+		</mesh>
+	);
 }
 
 /** doesn't interact with other particles (passes through them) */
 function NonInteractiveParticle({
-  pathToGLTF,
-  mass,
-  position,
-  ChildParticle,
-  numIcosahedronFaces,
-  pathToImage,
+	pathToGLTF,
+	mass,
+	position,
+	ChildParticle,
+	numIcosahedronFaces,
+	pathToImage,
 }) {
-  const ref = useRef();
-  useJitterParticle({
-    mass,
-    ref,
-  });
-  const scale = useStore((state) => state.scale);
+	const ref = useRef();
+	useJitterParticle({
+		mass,
+		ref,
+	});
+	const scale = useStore((state) => state.scale);
 
-  return (
-    <mesh ref={ref} scale={[scale, scale, scale]} position={position}>
-      <ChildParticle />
-    </mesh>
-  );
+	return (
+		<mesh
+			renderOrder={3}
+			ref={ref}
+			scale={[scale, scale, scale]}
+			position={position}
+		>
+			<ChildParticle />
+		</mesh>
+	);
 }
