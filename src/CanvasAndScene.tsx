d96ce32aea1b83b2225bfcdfb3fc9a7736fdfdbc
Daniel Corner <danielcorner7@gmail.com>
2021-01-13 02:31:04 -0500

    only show memstats once loaded

--- src/CanvasAndScene.tsx
+++ src/CanvasAndScene.tsx
@@ -3,19 +3,11 @@ import { Controls } from "react-three-gui";
 import Scene from "./components/Scene";
 import { useWindowSize } from "./utils/hooks";
 import * as THREE from "three";
-import MemoryStats from "react-memorystats";
-import { render } from "react-dom";
-import { useMount } from "./utils/utils";
+
 import { ScaleControls } from "./ScaleControls";
 // import { useFrame } from "react-three-fiber";
 import BottomControls from "./BottomControls";
 export default function CanvasAndScene({ renderProteins = true }) {
-  useMount(() => {
-    render(
-      <MemoryStats corner="topLeft" />,
-      document.querySelector("#memoryStats")
-    );
-  });
   const windowSize = useWindowSize();
 
   //  // This one makes the camera move in and out
--- src/components/LoadingIndicator.tsx
+++ src/components/LoadingIndicator.tsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { LinearProgress } from "@material-ui/core";
 import { useProgress } from "@react-three/drei";
 import styled from "styled-components/macro";
+import { render } from "react-dom";
+import MemoryStats from "react-memorystats";
 
 const StyledDiv = styled.div``;
 export function LoadingIndicator() {
   const { active, progress, errors, item, loaded, total } = useProgress();
+
+  // show memorystats once we're done loading
+  const alreadyRendered = useRef(false);
+  useEffect(() => {
+    if (progress === 100 && !alreadyRendered.current) {
+      alreadyRendered.current = true;
+      render(
+        <MemoryStats corner="topLeft" />,
+        document.querySelector("#memoryStats")
+      );
+    }
+  }, [progress]);
 
   return errors.length > 0 ? (
     JSON.stringify(errors)
