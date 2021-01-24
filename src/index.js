31168a6342a246201cd5871d6176a94006898249
Daniel Corner <danielcorner7@gmail.com>
2021-01-24 12:46:13 -0500

    add react-ga

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+// import reportWebVitals from "./reportWebVitals";
 import * as serviceWorker from "./serviceWorker";
+import ReactGA from "react-ga";
+ReactGA.initialize("G-RMP0BKW0EL");
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
